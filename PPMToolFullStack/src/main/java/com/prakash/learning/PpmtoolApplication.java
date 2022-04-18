c8be67461ee61f79991033701368528a3b6cbd8e
Prakash <prakasht2014@gmail.com>
2022-04-18 12:36:14 +0530

    Initial Commit

---
+++ PPMToolFullStack/src/main/java/com/prakash/learning/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.prakash.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
